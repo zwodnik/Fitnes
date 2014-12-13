fe7da4d83b5cfdb33e754b057df03f3b4371b570
Jechbeat <jechbeatdev@gmail.com>
2014-12-13 12:17:31 +0100

    llll

---
+++ Fitness/Fitness/Fitness.Shared/Logika.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitness
+{
+    class Logika
+    {
+        public int[] suma = new int[] { 0, 0, 0, 0, 0 };
+        public int index = 0;
+        public Logika()
+        {
+
+        }
+    }
+}
